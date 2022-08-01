7a897a0b78916f8b8e491e3404b611366be8b52f
Jianwei Zhang <13187409+zjwbupt@users.noreply.github.com>
2022-07-31 21:25:11 -0700

    Decorator: Learning Decorator Pattern

---
+++ src/Decorator/HouseBlend.java
@@ -0,0 +1,14 @@
+package Decorator;
+
+public class HouseBlend extends Beverage{
+
+
+    public HouseBlend() {
+        description = "HouseBlend";
+    }
+
+    public double cost() {
+        return 1.99;
+    }
+}
+
